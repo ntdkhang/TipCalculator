a98771bd0e696c54207f1acd35afb79e78093371
Khang <khang.nguyen.trd@gmail.com>
2021-11-22 10:19:37 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Nguyen Tran Duy Khang on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
